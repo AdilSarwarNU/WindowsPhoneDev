5a673a7f619aaa75e899aca4bb89c4d5b75e0ad4
Adil Sarwar <adil.sarwar11l4405@gmail.com>
2014-08-04 15:02:48 +0500

    Basic Xaml controls Project
    Lesson for learners to play with basic XAML controls and a few tips to
    be kept in mind while developing apps in their layouts

---
+++ b2_basicXAML/b2_basicXAML/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using b2_basicXAML.Resources;
+
+namespace b2_basicXAML
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
